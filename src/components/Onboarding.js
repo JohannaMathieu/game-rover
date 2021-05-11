2b53ce2353aceb8bb0e92431fc7bb0b89e3da6f3
Zahra Said <zahra@zahrasaid.org>
2021-05-11 22:49:21 +0200

    Add Css

--- src/components/Onboarding.js
+++ src/components/Onboarding.js
@@ -1,14 +1,18 @@
-import React from 'react'
-import "./Onboarding.css"
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./Onboarding.css";
+import { Link } from "react-router-dom";
 
 const Onboarding = () => {
-    return (
-        <div>
-            <h1>We are so bored!<br></br>Will you play with us?!</h1>
-            <button><Link to="/game">Take me to game</Link></button>
-        </div>
-    )
-}
+  return (
+    <div className="Onboarding">
+      <h1>
+        We are so bored!<br></br>Will you play with us?!
+      </h1>
+      <button>
+        <Link to="/game">Take me to game</Link>
+      </button>
+    </div>
+  );
+};
 
-export default Onboarding
+export default Onboarding;
