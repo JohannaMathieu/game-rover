ea932de3b8e0b454966914c4f0d25a0f802ab8a7
Zahra Said <zahra@zahrasaid.org>
2021-05-12 11:10:05 +0200

    Changed intro text on winning page

--- src/components/Chuck.js
+++ src/components/Chuck.js
@@ -5,7 +5,9 @@ import "./Chuck.css";
 const Chuck = (props) => {
   return (
     <div className="chuckstyle">
-      <h1>You won!!!</h1>
+      <h1>
+        You won!!! <br></br> Now that you found me, wanna hear a joke?
+      </h1>
       <p className="chuckjoke">{props.quote}</p>
       <button>
         <Link to="/game">Let's play again!</Link>
